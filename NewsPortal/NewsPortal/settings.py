db9d0b8cf48f130d14761a1b8deb839db602c291
LlamaUnicorn <holidaynight@gmail.com>
2022-05-26 07:07:11 +0300

    Installed Celery and Redis. Redis is not running yet.

--- NewsPortal/NewsPortal/settings.py
+++ NewsPortal/NewsPortal/settings.py
@@ -176,3 +176,9 @@ ADMINS = [
     ('IT', 'it@vokki.ru')
 ]
 SERVER_EMAIL = os.getenv('SERVER_EMAIL')  # это будет у нас вместо аргумента FROM в массовой рассылке
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
